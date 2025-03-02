d67f056954e188b4d01390af9e64d3557c414741
aNasiusA <kasserteea@gmail.com>
2025-03-02 12:38:10 +0000

    Create Express Backend Template

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { 
+    languageOptions: { globals: globals.browser }, 
+    env: { node: true }  // Enable Node.js environment
+  },
+  pluginJs.configs.recommended,
+];
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Base template for an Express backend
+
+1. Create two files in the root ".env.development.local" and ".env.production.local"
+
+2. In ".env.development.local" add the port, in .env.production.local add NODE_ENV="production"
