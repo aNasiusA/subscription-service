3b1e8246a3f7fcd1c0149d2a35e25459a51f3193
aNasiusA <kasserteea@gmail.com>
2025-03-04 15:07:46 +0000

    Add rate limiting and bot protecting (Arcject)

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjet from "@arcjet/node";
+import arcjectMiddleware from "./middlewares/arcject.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjectMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
